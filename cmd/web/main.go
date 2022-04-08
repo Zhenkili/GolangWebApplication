92c7380c64864afaad2d325eb9af0b20a94af818
“Zhenkili” <zhenqi024@gmail.com>
2022-04-07 23:34:51 -0700

    update main

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,16 +1,20 @@
 package main
 
+//update test
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/Zhenkili/udemyproject/pkg/handler"
 )
 
-const portNum = ":9090"
+const portNumber = ":8080"
 
+// main is the main function
 func main() {
 	http.HandleFunc("/", handler.Home)
 	http.HandleFunc("/about", handler.About)
 
-	_ = http.ListenAndServe(portNum, nil)
+	fmt.Println(fmt.Sprintf("Staring application on port %s", portNumber))
+	_ = http.ListenAndServe(portNumber, nil)
 }
