74cc62bf8771c9ff60795336b0754b52107842dd
“Zhenkili” <zhenqi024@gmail.com>
2022-05-05 23:33:12 -0700

    update routes

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -8,6 +8,7 @@ import (
 	"github.com/bmizerany/pat"
 )
 
+//use router instead of main
 func routes(app *config.Appconfig) http.Handler {
 	mux := pat.New()
 	///repalce under instead of the handlerFunc in main
